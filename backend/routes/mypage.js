1a9fe9fb16ef859131c3a8e0ff4ee731385d54b8
Cuzz <rygus9@naver.com>
2022-12-10 18:04:00 +0900

    내가 수정하면서 동작시킴

--- backend/routes/mypage.js
+++ backend/routes/mypage.js
@@ -1,46 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require("mongoose")
-const userModel = require('../models/users')
-const postingModel = require('../models/postings')
+const mongoose = require("mongoose");
+const userModel = require("../models/users");
+const postingModel = require("../models/postings");
+const express_session = require("express-session");
+const MongoStore = require("connect-mongo");
+const { googleSecret, sessionSecret } = require("../config/secret");
 
-let status
-const connectDB = async function(req,res,next){
-  try{
-    await mongoose.connect("mongodb://localhost:27017/websystemPj")
-    status = mongoose.connection.readyState
-    next()
-  }catch(err){
-    res.status(500).send("DB is unconnected.\n Can not use.")
+let status;
+const connectDB = async function (req, res, next) {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/websystemPj");
+    status = mongoose.connection.readyState;
+    next();
+  } catch (err) {
+    res.status(500).send("DB is unconnected.\n Can not use.");
   }
-}
+};
 
-router.use(connectDB)
+router.use(connectDB);
 
-router.get('/:userId', async function (req,res,next) {
-  let user = await userModel.findOne({userID: req.params.userId})
-  let list = user.list
-  let newList = []
-  for (i in list){
-    let listuser = await userModel.findOne({userID: list[i]})
-    if(user.mentor){
-      newList.push({menteeNickname: listuser.nickname, menteeProfile: listuser.profileImage})
-    }
-    else{
-      let post = await postingModel.findOne({userNickname: listuser.nickname})
-      newList.push({postId: post.postId, mentorNickname: listuser.nickname, menteeProfile: listuser.profileImage})
+router.use(
+  express_session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/websystemPj" }),
+    cookie: { maxAge: 3.6e6 * 24 * 14 },
+  })
+);
+
+router.get("/", async function (req, res, next) {
+  let user = await userModel.findOne({ userID: req.session.user.userID });
+  let list = user.list;
+  let newList = [];
+  for (i in list) {
+    let listuser = await userModel.findOne({ userID: list[i] });
+    if (user.mentor) {
+      newList.push({ menteeNickname: listuser.nickname, menteeProfile: listuser.profileImage });
+    } else {
+      let post = await postingModel.findOne({ userNickname: listuser.nickname });
+      newList.push({ postId: post.postId, mentorNickname: listuser.nickname, menteeProfile: listuser.profileImage });
     }
   }
 
-  return res.send({userProfile: user.profileImage, mlist: newList})
+  return res.send({ userProfile: user.profileImage, mlist: newList });
+});
 
-})
+router.get("/mentor/mypost", async function (req, res, next) {
+  const result = await postingModel.find({ mentorId: req.session.user.userID });
 
-router.get('/mentor/mypost', async function (req,res,next) {
-  const result = await postingModel.find({mentorId : req.session.user.userID});
-
-  if(result) res.status(200).send({contents : result, totalNum: result.length});
+  if (result) res.status(200).send({ contents: result, totalNum: result.length });
   else res.status(404).send(false);
-})
+});
 
 module.exports = router;
--- backend/routes/postings.js
+++ backend/routes/postings.js
@@ -1,159 +1,169 @@
 var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const express_session = require('express-session');
-const MongoStore = require('connect-mongo');
-const {sessionSecret} = require('../config/secret');
-const userModel = require('../models/users')
-const postingModel = require('../models/postings')
+const express_session = require("express-session");
+const MongoStore = require("connect-mongo");
+const { sessionSecret } = require("../config/secret");
+const userModel = require("../models/users");
+const postingModel = require("../models/postings");
 
 let status;
 
-const connectDB = async function(req,res,next){
-  try{
-    await mongoose.connect("mongodb://localhost:27017/websystemPj")
-    status = mongoose.connection.readyState
-    next()
-  } catch(err){
-    res.status(500).send("DB is unconnected.\n Can not use.")
+const connectDB = async function (req, res, next) {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/websystemPj");
+    status = mongoose.connection.readyState;
+    next();
+  } catch (err) {
+    res.status(500).send("DB is unconnected.\n Can not use.");
   }
-}
+};
 
 router.use(connectDB);
-router.use(express_session({
-  secret: sessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({mongoUrl:"mongodb://localhost:27017/websystemPj"}),
-  cookie:{maxAge:(3.6e+6)*24*14}
-}))
+router.use(
+  express_session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/websystemPj" }),
+    cookie: { maxAge: 3.6e6 * 24 * 14 },
+  })
+);
 
 /* POST /postings */
-router.post('/', async function(req, res, next) {
+router.post("/", async function (req, res, next) {
   let randomId;
-  while(true) {
-      randomId = (Math.floor(Math.random() * 10000)).toString();
-      let check = await postingModel.findOne({postId : randomId});
-      if(!check) break;
+  while (true) {
+    randomId = Math.floor(Math.random() * 10000).toString();
+    let check = await postingModel.findOne({ postId: randomId });
+    if (!check) break;
   }
 
-  const user = await userModel.findOne({userID : req.session.user.userID});
+  const user = await userModel.findOne({ userID: req.session.user.userID });
 
-  postingModel.create({
-      postId : randomId,
-      mentorId : req.session.user.userID,
-      title : req.body.title,
-      content : req.body.content,
-      userNickname : req.session.user.nickname,
-      userProfile : user.profileImage,
-      grade : user.grade,
-      mentormajor : user.major,
-      star : "0",
-      date: new Date()
-  }, function(err) {
-      if(err) res.status(500).send(false);
+  postingModel.create(
+    {
+      postId: randomId,
+      mentorId: req.session.user.userID,
+      title: req.body.title,
+      content: req.body.content,
+      userNickname: req.session.user.nickname,
+      userProfile: user.profileImage,
+      grade: user.grade,
+      mentormajor: user.major,
+      star: "0",
+      date: new Date(),
+    },
+    function (err) {
+      if (err) res.status(500).send(false);
       else res.status(200).send(randomId);
-  });
+    }
+  );
 });
 
 /* PUT /postings/:post_id */
-router.put('/:post_id', async function(req, res, next) { 
-  const check = await postingModel.findOne({postId : req.params.post_id});
-  if(!check) { // 새로운 id
+router.put("/:post_id", async function (req, res, next) {
+  const check = await postingModel.findOne({ postId: req.params.post_id });
+  if (!check) {
+    // 새로운 id
     res.status(404).send(false);
-  }
-  else { // 기존 post 수정
-      postingModel.updateOne({postId : req.params.post_id}, {
-          title: req.body.title,
-          content: req.body.content,
-          date: new Date()
-      }, function(err) {
-          if(err) res.status(500).send(false);
-          else {
-              res.status(200).send(true);
-          }
-      });
+  } else {
+    // 기존 post 수정
+    postingModel.updateOne(
+      { postId: req.params.post_id },
+      {
+        title: req.body.title,
+        content: req.body.content,
+        date: new Date(),
+      },
+      function (err) {
+        if (err) res.status(500).send(false);
+        else {
+          res.status(200).send(true);
+        }
+      }
+    );
   }
 });
 
 /* GET /postings */
-router.get('/', async function(req, res, next) {
-  let targetCategory = req.body.category;
-  let sortTarget = req.body.sort;
-  let page = req.body.page;
-  if(page === undefined) page = 0;
+router.get("/", async function (req, res, next) {
+  let targetCategory = req.query.category || "소프트웨어학과";
+  let sortTarget = req.query.sort || "최신순";
+  let page = req.query.page;
+  if (page === undefined) page = 0;
 
   let result;
 
   if (sortTarget === "별점순") {
-      result = await postingModel.find({mentormajor : targetCategory}).sort({ "star": -1 });
-  } else { // 최신순
-      result = await postingModel.find({mentormajor : targetCategory}).sort({ "date": -1 });
+    result = await postingModel.find({ mentormajor: targetCategory }).sort({ star: -1 });
+  } else {
+    // 최신순
+    result = await postingModel.find({ mentormajor: targetCategory }).sort({ date: -1 });
   }
 
-  if(result) res.send({contents: result.slice(12*page, (12*page + 12)), totalNum: result.length});
+  if (result) res.send({ contents: result.slice(12 * page, 12 * page + 12), totalNum: result.length });
   else res.status(404).send(false);
 });
 
 /* GET /postings/:post_id */
-router.get('/:post_id', async function(req, res, next) {
-  const result = await postingModel.findOne({postId : req.params.post_id});
+router.get("/:post_id", async function (req, res, next) {
+  const result = await postingModel.findOne({ postId: req.params.post_id });
 
-  if(result) res.status(200).send(result);
+  if (result) res.status(200).send(result);
   else res.status(404).send(false);
 });
 
 /* DELETE /postings/:post_id */
-router.delete('/:post_id', async function(req, res, next) {
-  const check = await postingModel.findOne({postId : req.params.post_id});
-  if(!check) {
+router.delete("/:post_id", async function (req, res, next) {
+  const check = await postingModel.findOne({ postId: req.params.post_id });
+  if (!check) {
     res.status(404).send(false);
     return;
   }
 
-  postingModel.deleteOne({postId : req.params.post_id}, function(err) {
-    if(err) res.status(500).send(false);
+  postingModel.deleteOne({ postId: req.params.post_id }, function (err) {
+    if (err) res.status(500).send(false);
     else res.status(200).send(true);
-  })
-});
-
-      postingModel.deleteOne({ postId: req.params.post_id }, function (err) {
-        if (err) res.status(500).send(false);
-        else res.status(200).send(true);
-      });
-    },
-    (err) => {
-      res.status(500).send("error : DB is not connected.");
-    }
-  );
+  });
 });
 
 // 멘토링 신청하기..?
 /* POST /postings/:post_id */
 //const model = mongoose.model("Users",websystemPj)
 
-router.post('/:postId', async function(req, res, next) {
-  const {postid} = req.body
+router.post("/:postId", async function (req, res, next) {
+  const { postid } = req.body;
 
-  let post = await postingModel.find({postId: postid})
-  let mentee = await userModel.find({userID: req.session.user.userID, password: req.session.user.password})
-  let mentor = await userModel.find({nickname: post[0].userNickname, profileImage: post[0].userProfile, major:post[0].mentormajor})
+  let post = await postingModel.find({ postId: postid });
+  let mentee = await userModel.find({ userID: req.session.user.userID, password: req.session.user.password });
+  let mentor = await userModel.find({
+    nickname: post[0].userNickname,
+    profileImage: post[0].userProfile,
+    major: post[0].mentormajor,
+  });
 
   if (mentee[0].list.indexOf(mentor[0].userID) >= 0)
-    return res.status(400).send({result:false, message:"이미 신청한 멘토입니다."})
+    return res.status(400).send({ result: false, message: "이미 신청한 멘토입니다." });
 
-  let mentorList = mentee[0].list
-  mentorList.push(mentor[0].userID)
+  let mentorList = mentee[0].list;
+  mentorList.push(mentor[0].userID);
   //console.log(mentorList);
-  let updateMentee = await userModel.updateOne({userID: req.session.user.userID, password: req.session.user.password},{list:mentorList})
+  let updateMentee = await userModel.updateOne(
+    { userID: req.session.user.userID, password: req.session.user.password },
+    { list: mentorList }
+  );
 
-  let menteeList = mentor[0].list
-  menteeList.push(mentee[0].userID)
+  let menteeList = mentor[0].list;
+  menteeList.push(mentee[0].userID);
   //console.log(menteeList);
-  let updateMentor = await userModel.updateOne({nickname: post[0].userNickname, profileImage: post[0].userProfile, major:post[0].mentormajor},{list:menteeList})
+  let updateMentor = await userModel.updateOne(
+    { nickname: post[0].userNickname, profileImage: post[0].userProfile, major: post[0].mentormajor },
+    { list: menteeList }
+  );
 
-  return res.send({result: true, mentorList: updateMentee.list, menteeLIst: updateMentor.list})
+  return res.send({ result: true, mentorList: updateMentee.list, menteeLIst: updateMentor.list });
 });
 
 module.exports = router;
