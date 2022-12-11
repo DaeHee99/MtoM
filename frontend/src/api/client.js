19ff89538de8a384a747258db7c6563ac5c58b9b
Cuzz <rygus9@naver.com>
2022-12-11 14:54:06 +0900

    포트 80 으로 변경

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const client = axios.create();
 
 // client.defaults.baseURL = "http://localhost:3000";
-client.defaults.baseURL = "http://localhost:3080";
+client.defaults.baseURL = "http://localhost:80";
 
 export default client;
