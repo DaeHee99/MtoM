0b03c9c2b7e8c3962f60f239e091ada039a929b3
Cuzz <rygus9@naver.com>
2022-12-11 15:11:25 +0900

    프론트 빌드가 너무 오래걸리네요.

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const client = axios.create();
 
 // client.defaults.baseURL = "http://localhost:3000";
-client.defaults.baseURL = "http://http://ec2-3-36-86-195.ap-northeast-2.compute.amazonaws.com:3080/";
+client.defaults.baseURL = "http://http://ec2-3-36-86-195.ap-northeast-2.compute.amazonaws.com/";
 
 export default client;
