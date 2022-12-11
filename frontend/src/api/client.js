2423b4b778d333b676d8dc5aaee628a2cae13926
Cuzz <rygus9@naver.com>
2022-12-11 14:39:58 +0900

    배포용 빌드 파일도 올림

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client = axios.create();
 
-client.defaults.baseURL = "http://localhost:3000";
-// client.defaults.baseURL = "http://localhost:3080";
+// client.defaults.baseURL = "http://localhost:3000";
+client.defaults.baseURL = "http://localhost:3080";
 
 export default client;
