af2cf57582a8e4a2df6c59e61c12fc01a5a8a3ec
Cuzz <rygus9@naver.com>
2022-12-11 15:15:16 +0900

    실수 그만...

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const client = axios.create();
 
 // client.defaults.baseURL = "http://localhost:3000";
-client.defaults.baseURL = "http://http://ec2-3-36-86-195.ap-northeast-2.compute.amazonaws.com:80";
+client.defaults.baseURL = "http://ec2-3-36-86-195.ap-northeast-2.compute.amazonaws.com";
 
 export default client;
